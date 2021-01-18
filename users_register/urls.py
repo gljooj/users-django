ae803edffc76063f76c1a85ea91b1d86cbb7c74a
Gabbe-Menu <gabriel.jesus@menu.com.vc>
2021-01-18 09:32:27 -0300

    Fix urls

--- users_register/urls.py
+++ users_register/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.users_list, name="user_list"),
-    path('<str:id>/', views.user_edit, name="user_edit"),
-    path('new', views.user_new, name="user_new"),
+    path('user/<str:id>/', views.user_edit, name="user_edit"),
+    path('user', views.user_new, name="user_new"),
     path('delete/<str:id>/', views.user_delete, name="user_delete"),
 ]
